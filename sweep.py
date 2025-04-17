afbf4c527ff54d69ba4097909e44a3af5d147026
Andrew Fitzgibbon <awf@fitzgibbon.ie>
2025-04-17 08:57:49 +0000

    orthogonalize sweep.py

--- sweep.py
+++ sweep.py
@@ -1,28 +1,11 @@
 import sys
 from awfutils import MkSweep
 
-assert len(sys.argv) == 2, "Usage: python sweep.py <sweepdir>"
+assert len(sys.argv) == 2, "Usage: python sweep.py <sweepdir> < commands.txt"
 sweepdir = sys.argv[1]
 
 with MkSweep(sweepdir) as ms:
-    cmd = "python train.py config/train_shakespeare_char.py "
+    for line in sys.stdin:
+        ms.add(line.rstrip())
 
-    defaults = dict(
-        dtype="bfloat16",
-    )
-
-    ms.add(cmd, defaults, qat="bfloat16")
-    ms.add(cmd, defaults, qat="float16")
-
-    for seed in range(5):
-        ms.add(cmd, defaults, seed=seed + 42, qat_rnd="tne")
-        for sr in ("sr", "srf", "srff"):
-            for srnumbits in (1, 2, 3, 4, 8):
-                ms.add(
-                    cmd,
-                    defaults,
-                    seed=seed + 42,
-                    qat="b8p4",
-                    qat_rnd=sr,
-                    qat_srn=srnumbits,
-                )
+print("make -f", ms.makefile_path, "-j 2")
